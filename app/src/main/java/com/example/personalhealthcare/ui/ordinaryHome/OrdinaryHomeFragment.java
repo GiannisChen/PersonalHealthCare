93ddf7aef327dc3b135eefeeca57bac2c9cd6308
GiannisChen <giannischen@foxmail.com>
2020-11-13 21:00:20 +0800

    admin page backup

--- app/src/main/java/com/example/personalhealthcare/ui/ordinaryHome/OrdinaryHomeFragment.java
+++ app/src/main/java/com/example/personalhealthcare/ui/ordinaryHome/OrdinaryHomeFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -21,7 +23,23 @@ public class OrdinaryHomeFragment extends Fragment {
     private Integer UserID;
 
     private OrdinaryHomeViewModel ordinaryHomeViewModel;
+
     private QMUITopBarLayout mTopBar;
+
+    private ImageView itemFoodImage1;
+    private ImageView itemFoodImage2;
+    private ImageView itemFoodImage3;
+    private ImageView itemFoodImage4;
+
+    private TextView itemFoodName1;
+    private TextView itemFoodName2;
+    private TextView itemFoodName3;
+    private TextView itemFoodName4;
+
+    private TextView itemFoodCalories1;
+    private TextView itemFoodCalories2;
+    private TextView itemFoodCalories3;
+    private TextView itemFoodCalories4;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,13 +59,28 @@ public class OrdinaryHomeFragment extends Fragment {
         mTopBar = root.findViewById(R.id.topbar);
         mTopBar.setTitle("首页");
 
-        initView();
+        initView(root);
         initTopBar();
 
         return root;
     }
 
-    private void initView() {
+    private void initView(View root) {
+        itemFoodImage1 = root.findViewById(R.id.item_img_1);
+        itemFoodImage2 = root.findViewById(R.id.item_img_2);
+        itemFoodImage3 = root.findViewById(R.id.item_img_3);
+        itemFoodImage4 = root.findViewById(R.id.item_img_4);
+
+        itemFoodName1 = root.findViewById(R.id.item_food_name_1);
+        itemFoodName2 = root.findViewById(R.id.item_food_name_2);
+        itemFoodName3 = root.findViewById(R.id.item_food_name_3);
+        itemFoodName4 = root.findViewById(R.id.item_food_name_4);
+
+        itemFoodCalories1 = root.findViewById(R.id.item_food_calories_1);
+        itemFoodCalories2 = root.findViewById(R.id.item_food_calories_2);
+        itemFoodCalories3 = root.findViewById(R.id.item_food_calories_3);
+        itemFoodCalories4 = root.findViewById(R.id.item_food_calories_4);
+
 
     }
 
