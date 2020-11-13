6bfd27e1a0fd7cc8be2fc17efb6650034e62f6ba
GiannisChen <giannischen@foxmail.com>
2020-11-13 16:06:51 +0800

    admin page image-add

---
+++ app/src/main/java/com/example/personalhealthcare/CommonUtils/ImageUtils.java
@@ -0,0 +1,23 @@
+package com.example.personalhealthcare.CommonUtils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+    public static byte[] Bitmap2Bytes(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Bitmap tmp = Bitmap.createScaledBitmap(bitmap, 250, 250, true);
+        tmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        return baos.toByteArray();
+    }
+
+    public static Bitmap Bytes2Bitmap(byte[] bytes) {
+        if(bytes.length != 0) {
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        } else {
+            return null;
+        }
+    }
+}
